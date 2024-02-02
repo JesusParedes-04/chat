c27a8f006588c21c95d4ff3a1f7565b4e035dd9a
Jesús Paredes <jjesus.paredesl@gmail.com>
2024-02-02 14:46:44 -0300

    chat

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const REMOTE_MONGO_URL = process.env.REMOTE_MONGO_URL;
-export const PORT = process.env.PORT
+export const PORT = process.env.PORT || 8080
